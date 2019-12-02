14f002e1016c800b90c50ae5ddafd26476cd2ac4
ac47kk <ac47kkmaa@gmail.com>
2019-12-02 12:16:03 +0900

    SecondKadaiAppのラベルを追加した。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,11 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func next(_ sender: Any) {
+        let x = String(self.name.text ?? "")
+        print(x)
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    @IBOutlet weak var name: UITextField!
+    
+    @IBAction func name(_ sender: Any) {
+    }
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            // segueから遷移先のResultViewControllerを取得する
+    let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        resultViewController.x = String(self.name.text ?? "")
+        
     }
 
-
 }
-
